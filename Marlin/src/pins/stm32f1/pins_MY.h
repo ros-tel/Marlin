1b454dd2487488ce3d86a01147b1126fbde4d7eb
ros-tel <vladvladow@yandex.ru>
2025-05-07 10:38:39 +0500

    Перенос на другие пины

--- Marlin/src/pins/stm32f1/pins_MY.h
+++ Marlin/src/pins/stm32f1/pins_MY.h
@@ -39,42 +39,42 @@
 //
 // Limit Switches
 //
-#define X_STOP_PIN PA0
-#define Y_STOP_PIN PA1
+#define X_STOP_PIN     PA0
+#define Y_STOP_PIN     PA1
 
 //
 // Steppers
 //
 // X & Y enable are the same
-#define X_STEP_PIN PB7
-#define X_DIR_PIN  PB6
-#define X_ENABLE_PIN PB9
+#define X_STEP_PIN     PB7
+#define X_DIR_PIN      PB6
+#define X_ENABLE_PIN   PB9
 
-#define Y_STEP_PIN PA10
-#define Y_DIR_PIN  PA9
-#define Y_ENABLE_PIN PA12
+#define Y_STEP_PIN     PA9
+#define Y_DIR_PIN      PA8
+#define Y_ENABLE_PIN   PA15
 
 //#define TMC_USE_SW_SPI
-#define TMC_SPI_MOSI  PB5
-#define TMC_SPI_MISO  PB4
-#define TMC_SPI_SCK   PB3
+#define TMC_SPI_MOSI   PB5
+#define TMC_SPI_MISO   PB4
+#define TMC_SPI_SCK    PB3
 
-#define X_CS_PIN PB8
-#define Y_CS_PIN PA11
+#define X_CS_PIN       PB8
+#define Y_CS_PIN       PA10
 
 // Laser PWM
-#define SPINDLE_LASER_PWM_PIN PA8
+#define SPINDLE_LASER_PWM_PIN PB15
 
 
 //Encoder
-#define BTN_EN1           PC15
-#define BTN_EN2           PC14
-#define BTN_ENC           PC13
+#define BTN_EN1        PC15
+#define BTN_EN2        PC14
+#define BTN_ENC        PC13
 
 // SDCard
-#define SDSS               PA4
-#define SD_DETECT_PIN     -1
-#define KILL_PIN          -1
+#define SDSS           PA4
+#define SD_DETECT_PIN -1
+#define KILL_PIN      -1
 
 // LCD REPRAP_DISCOUNT_SMART_CONTROLLER
 //#define LCD_PINS_RS         PB12
@@ -86,10 +86,10 @@
 
 //MKS_MINI_12864
 //
-#define DOGLCD_CS         PA3
-#define DOGLCD_A0         PB10
+#define DOGLCD_CS     PA3
+#define DOGLCD_A0     PB10
 
-#define NEOPIXEL_PIN         PB11
+#define NEOPIXEL_PIN  PB11
 
  // GLCD features
  //
