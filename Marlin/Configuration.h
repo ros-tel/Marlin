000ad79ab171a46986aa4fc9d83af12e34d481c9
ros-tel <vladvladow@yandex.ru>
2025-06-06 21:45:41 +0500

    Ускорение и задержка на розжиг уменьшены

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1216,7 +1216,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 3000, 3000 }
+#define DEFAULT_MAX_ACCELERATION      { 300, 300 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
