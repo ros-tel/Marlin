c9e7984c6898ff63a53f902bcdb4eca101c42b89
ros-tel <vladvladow@yandex.ru>
2025-06-12 17:31:35 +0500

    Включение комментов скобками

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3714,7 +3714,7 @@
  * Note that G0 feedrates should be used with care for 3D printing (if used at all).
  * High feedrates may cause ringing and harm print quality.
  */
-//#define PAREN_COMMENTS      // Support for parentheses-delimited comments
+#define PAREN_COMMENTS      // Support for parentheses-delimited comments
 #define GCODE_MOTION_MODES  // Remember the motion mode (G0 G1 G2 G3 G5 G38.X) and apply for X Y Z E F, etc.
 
 // Enable and set a (default) feedrate for all G0 moves
